db01d0f3c455ea6f0521d8593de4c91408ef006c
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-09-03 16:36:10 -0300

    Estruturado main.jsx em pasta src para a renderização da LandinPage

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {LandingPage} from './pages/landing-page'
+import './styles-global/global.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LandingPage/>
+  </React.StrictMode>
+)
