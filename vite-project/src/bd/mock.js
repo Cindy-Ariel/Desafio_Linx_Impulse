b194a81beb400ddf54ad8080b062157d05d50c8c
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-09-04 21:39:14 -0300

    Construído Mock da API

---
+++ vite-project/src/bd/mock.js
@@ -0,0 +1,199 @@
+export const page1 = [
+  {
+    id: 0,
+    name: 'Product Number 0',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399',
+    oldPrice: 184,
+    price: 140,
+    description: 'Product long description number 0 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 14,
+    },
+  },
+  {
+    id: 0,
+    name: 'Product Number 0',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399',
+    oldPrice: 184,
+    price: 140,
+    description: 'Product long description number 0 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 14,
+    },
+  },
+  {
+    id: 0,
+    name: 'Product Number 0',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399',
+    oldPrice: 184,
+    price: 140,
+    description: 'Product long description number 0 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 14,
+    },
+  },
+  {
+    id: 0,
+    name: 'Product Number 0',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399',
+    oldPrice: 184,
+    price: 140,
+    description: 'Product long description number 0 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 14,
+    },
+  },
+  {
+    id: 0,
+    name: 'Product Number 0',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399',
+    oldPrice: 184,
+    price: 140,
+    description: 'Product long description number 0 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 14,
+    },
+  },
+  {
+    id: 0,
+    name: 'Product Number 0',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399',
+    oldPrice: 184,
+    price: 140,
+    description: 'Product long description number 0 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 14,
+    },
+  },
+  {
+    id: 0,
+    name: 'Product Number 0',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399',
+    oldPrice: 184,
+    price: 140,
+    description: 'Product long description number 0 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 14,
+    },
+  },
+  {
+    id: 0,
+    name: 'Product Number 0',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6747399',
+    oldPrice: 184,
+    price: 140,
+    description: 'Product long description number 0 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 14,
+    },
+  },
+
+]
+
+export const page2 = [
+
+  {
+    id: 1,
+    name: 'Product Number 1',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538',
+    oldPrice: 212,
+    price: 176,
+    description: 'Product long description number 1 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 17.6,
+    },
+  },
+  {
+    id: 1,
+    name: 'Product Number 1',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538',
+    oldPrice: 212,
+    price: 176,
+    description: 'Product long description number 1 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 17.6,
+    },
+  },
+  {
+    id: 1,
+    name: 'Product Number 1',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538',
+    oldPrice: 212,
+    price: 176,
+    description: 'Product long description number 1 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 17.6,
+    },
+  },
+  {
+    id: 1,
+    name: 'Product Number 1',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538',
+    oldPrice: 212,
+    price: 176,
+    description: 'Product long description number 1 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 17.6,
+    },
+  },
+  {
+    id: 1,
+    name: 'Product Number 1',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538',
+    oldPrice: 212,
+    price: 176,
+    description: 'Product long description number 1 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 17.6,
+    },
+  },
+  {
+    id: 1,
+    name: 'Product Number 1',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538',
+    oldPrice: 212,
+    price: 176,
+    description: 'Product long description number 1 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 17.6,
+    },
+  },
+  {
+    id: 1,
+    name: 'Product Number 1',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538',
+    oldPrice: 212,
+    price: 176,
+    description: 'Product long description number 1 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 17.6,
+    },
+  },
+  {
+    id: 1,
+    name: 'Product Number 1',
+    image: 'imagens.pontofrio.com.br/Control/ArquivoExibir.aspx?IdArquivo=6670538',
+    oldPrice: 212,
+    price: 176,
+    description: 'Product long description number 1 just to make more than one like of text.',
+    installments: {
+      count: 10,
+      value: 17.6,
+    },
+  },
+]
