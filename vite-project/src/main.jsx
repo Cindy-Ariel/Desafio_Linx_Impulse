19fe987140d21efac016a8c1002dd0809a7126ca
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-09-04 21:41:00 -0300

    Alterado forma de importação

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {LandingPage} from './pages/landing-page'
+import LandingPage from './pages/landing-page'
 import './styles-global/global.css'
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LandingPage/>
-  </React.StrictMode>
+    <LandingPage />
+  </React.StrictMode>,
 )
