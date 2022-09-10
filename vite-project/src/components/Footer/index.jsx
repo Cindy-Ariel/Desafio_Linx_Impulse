d68d753347a1e661acc0320ba11ae13f60bf54ba
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-09-10 20:31:27 -0300

    Estilizando o componente Footer

--- vite-project/src/components/Footer/index.jsx
+++ vite-project/src/components/Footer/index.jsx
@@ -3,9 +3,16 @@ import S from './footer.module.css'
 export function Footer() {
   return (
     <div className={S.footer}>
-      <h1 className={S.titulo_footer}>
-        Testando suas habilidades em HTML, CSS e JS. Linx Impulse 2019
-      </h1>
+      <div className={S.box_titulo_footer}>
+        <h1 className={S.titulo_footer}>
+          Testando suas habilidades em HTML, CSS e JS.
+          <br />
+          Linx Impulse
+          <br />
+          2019
+        </h1>
+      </div>
+
     </div>
 
   )
